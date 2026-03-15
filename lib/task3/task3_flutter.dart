34ca9c5098979819702390901f22c833484bd09d
fatimahawny <fatimahawny@gmail.com>
2026-03-15 14:14:30 +0200

    task_3_flutter

---
+++ lib/task3/task3_flutter.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Task3Flutter extends StatelessWidget {
+  const Task3Flutter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
